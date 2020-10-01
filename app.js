f7bda0f6d4a294ab0ff253c95588135ee29d8a45
ryan-puffer <ryanpufferdev@gmail.com>
2020-10-01 15:02:53 -0500

    add card styling

--- app.js
+++ app.js
@@ -15,20 +15,33 @@ function displayCoins(coinData){
     coins = coinData;
     console.log(coins);
     let coinHTML = '';
-    coins.forEach(coin => {
+    coins.forEach((coin, index) => {
         let coinName = coin.name;
+        let id = coin.id;
         let symbol = coin.symbol;
         let current = coin.current_price;
         let change = coin.price_change_percentage_24h;
+        let image = coin.image;
         coinHTML += `
         <div class="card">
-            <div class="card-name">
-                <h2 class="name">${coinName}</h2>
-                <h2 class="symbol">${symbol}</h2>
+            <h2 class="name">${coinName}</h2>
+            <h2 class="symbol">${symbol}</h2>
+            <div class="card-image">
+                <img class="logo" src="${image}">
             </div>
-            <div class="card-price">
-                <h3 class="price">Current Price<span>${current}</span></h3>
-                <h3 class="24hr">24HR Change <span>${change}</span></h3>
+            <div class="price-section">
+                <div class="card-price">
+                    <h3 class="price">Current Price</h3>
+                    <h3 class="price">$ ${current}</h3>
+                </div>
+                <div class="card-price">
+                    <h3 class="price">24hr Change</h3>
+                    <h3 class="price">${change}%</h3>
+                </div>
+            </div>
+            <div class="card-info">
+                <a href="https://www.coingecko.com/en/coins/${id}" class="btn">View on CoinGecko</a>
+               
             </div>
         </div>
             `
